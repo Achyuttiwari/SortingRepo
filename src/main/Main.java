e0b951fd464170a96ba4d14fabbf24d9d727f4c0
Achyut Kumar Tiwari <achyuttiwari34@gmail.com>
2024-04-07 13:44:06 +0530

    For loop for unsorted array initializing randomly between 1 to 100.

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,6 +9,9 @@ public class Main {
         final Scanner scanner  = new Scanner(System.in);
         int sizeOfArray = scanner.nextInt();
         int[] array = new int[sizeOfArray];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int)(Math.random() * 100);
+        }
 
     }
 }
