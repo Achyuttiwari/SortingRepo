8347ed45f6cca60f4186716bc4e9bd85d12a57d4
Achyut Kumar Tiwari <achyuttiwari34@gmail.com>
2024-04-08 23:55:46 +0530

    In this line its print whether the unsorted array will be printed in descending or ascending order by ternary operator.

--- src/bubbleSort/BubbleSort.java
+++ src/bubbleSort/BubbleSort.java
@@ -3,6 +3,6 @@ package bubbleSort;
 public class BubbleSort {
 
     public void sort(int[] array, int choiceForSorting) {
-        System.out.println(choiceForSorting == 1 ? "DESCENDING ORDER SORT" : "ASCENDING ORDER SORT");
+        System.out.println(choiceForSorting == 1 ? "DESCENDING ORDER SORT " : "ASCENDING ORDER SORT ");
     }
 }
