d0955dff505e475da21184a6eaee4efa0b2d162d
Achyut Kumar Tiwari <achyuttiwari34@gmail.com>
2024-04-09 00:21:02 +0530

    this will print the message array is sorted using bubble sort

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,12 +16,13 @@ public class Main {
         }
         System.out.println("Unsorted Array");
         System.out.println(Arrays.toString(array));
-        System.out.println("Press 1 to sort this array using Bubble sort..!!");
+        System.out.println("Press 1 to sort this array using Bubble Sort.!!");
         int choice = scanner.nextInt();
         switch(choice){
             case 1: /*BUBBLE SORT*/
                 BubbleSort bubblesort = new BubbleSort();
                 bubblesort.sort(array, (int) Math.ceil(Math.random()));
+                System.out.println("ARRAY SORTED BY BUBBLE SORT");
 
         }
 
