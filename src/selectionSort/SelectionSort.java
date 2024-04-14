5226b85ebb876e9c7612cc5b6fc73e6bc96ae95b
Achyut Kumar Tiwari <achyuttiwari34@gmail.com>
2024-04-14 16:54:56 +0530

    posMin is for finding position of minimum value.

--- src/selectionSort/SelectionSort.java
+++ src/selectionSort/SelectionSort.java
@@ -3,7 +3,7 @@ package selectionSort;
 public class SelectionSort {
     public void sort(int[] array) {
         for (int fill = 0; fill < array.length; fill++) {
-            
+            int poseMin = fill;
         }
 
 
