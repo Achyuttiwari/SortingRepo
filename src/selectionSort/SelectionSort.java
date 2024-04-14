c15ae6efefd95a305225fbe2a120541b1d669def
Achyut Kumar Tiwari <achyuttiwari34@gmail.com>
2024-04-14 20:32:52 +0530

    if condition is to check the next element is smaller than the previous one and if it is then posMin will become next.

--- src/selectionSort/SelectionSort.java
+++ src/selectionSort/SelectionSort.java
@@ -5,7 +5,7 @@ public class SelectionSort {
         for (int fill = 0; fill < array.length; fill++) {
             int poseMin = fill;
             for (int next = fill + 1; next < array.length - 1; next++) {
-
+                if(array[next] < array[fill]) poseMin = next;
             }
         }
 
