4b86fba21c241ac01b429bd7ad0cd6376d13d5bd
Achyut Kumar Tiwari <achyuttiwari34@gmail.com>
2024-04-14 20:40:42 +0530

    swap method is for swap the smallest element in the array.

--- src/selectionSort/SelectionSort.java
+++ src/selectionSort/SelectionSort.java
@@ -5,10 +5,13 @@ public class SelectionSort {
         for (int fill = 0; fill < array.length; fill++) {
             int poseMin = fill;
             for (int next = fill + 1; next < array.length - 1; next++) {
-                if(array[next] < array[fill]) poseMin = next;
+                if (array[next] < array[fill]) poseMin = next;
             }
-        }
 
+        swap(array, fill, poseMin);
+    }
+    }
+    private void swap(int[] array, int fill, int poseMin){
 
     }
 }
