37cbaaa5527a6f677f25de4d105476bf028058f7
Achyut Kumar Tiwari <achyuttiwari34@gmail.com>
2024-04-14 20:45:53 +0530

    swapped

--- src/selectionSort/SelectionSort.java
+++ src/selectionSort/SelectionSort.java
@@ -4,14 +4,16 @@ public class SelectionSort {
     public void sort(int[] array) {
         for (int fill = 0; fill < array.length; fill++) {
             int poseMin = fill;
-            for (int next = fill + 1; next < array.length - 1; next++) {
-                if (array[next] < array[fill]) poseMin = next;
+            for (int next = fill + 1; next < array.length; next++) {
+                if (array[next] < array[poseMin]) poseMin = next;
             }
 
         swap(array, fill, poseMin);
     }
     }
     private void swap(int[] array, int fill, int poseMin){
-
+        int temp =array[fill];
+        array[fill] = array[poseMin];
+        array[poseMin] = temp;
     }
 }
