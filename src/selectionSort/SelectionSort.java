ca8f44b93b0998723f5e940c9f19dbd40f0c73f0
Achyut Kumar Tiwari <achyuttiwari34@gmail.com>
2024-04-14 20:31:03 +0530

    to search the smallest element in the array.

--- src/selectionSort/SelectionSort.java
+++ src/selectionSort/SelectionSort.java
@@ -4,7 +4,9 @@ public class SelectionSort {
     public void sort(int[] array) {
         for (int fill = 0; fill < array.length; fill++) {
             int poseMin = fill;
+            for (int next = fill + 1; next < array.length - 1; next++) {
 
+            }
         }
 
 
