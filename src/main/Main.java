dc3d13ca951955fc216a1b95cac00882ab6b86d1
Achyut Kumar Tiwari <achyuttiwari34@gmail.com>
2024-04-20 23:29:09 +0530

    print message to sort the array in insertion sort

--- src/main/Main.java
+++ src/main/Main.java
@@ -20,6 +20,7 @@ public class Main {
         System.out.println(Arrays.toString(array));
         System.out.println("Press 1 to sort this array using Bubble Sort.!!");
         System.out.println("Press 2 to sort this array in Selection Sort.!!");
+        System.out.println("Press 3 to sort this array in Insertion Sort.!!");
         int choice = scanner.nextInt();
         switch(choice){
             case 1: /*BUBBLE SORT*/
