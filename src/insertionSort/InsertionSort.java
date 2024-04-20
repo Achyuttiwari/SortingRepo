4cbe1a37d566be6cfd9faa29f347d097b1835bab
Achyut Kumar Tiwari <achyuttiwari34@gmail.com>
2024-04-20 23:45:54 +0530

    going to create static insert method

--- src/insertionSort/InsertionSort.java
+++ src/insertionSort/InsertionSort.java
@@ -4,7 +4,7 @@ public class InsertionSort {
 
     public void sort(int[] array) {
         for (int nextIndex = 1; nextIndex < array.length; nextIndex++) {
-
+            insert(array, nextIndex);
         }
     }
 }
