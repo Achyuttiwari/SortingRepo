0d6654813daac9eaaa2896d74bd415ef5b6a28d3
Achyut Kumar Tiwari <achyuttiwari34@gmail.com>
2024-04-20 23:49:11 +0530

    ...!!

--- src/insertionSort/InsertionSort.java
+++ src/insertionSort/InsertionSort.java
@@ -10,6 +10,11 @@ public class InsertionSort {
 
     static void insert(int[] array, int nextIndex) {
         int nextValue = array[nextIndex];
+        while (nextIndex > 0 && nextValue < array[nextIndex - 1]) {
+            array[nextIndex] = array[nextIndex - 1];
+            nextIndex--;
+        }
+        array[nextIndex] = nextValue;
     }
 
 }
