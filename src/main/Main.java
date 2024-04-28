ff9f8c8444c7bc23138bcc30729aab750b989b57
Achyut Kumar Tiwari <achyuttiwari34@gmail.com>
2024-04-28 18:04:48 +0530

    print message as follows

--- src/main/Main.java
+++ src/main/Main.java
@@ -22,6 +22,7 @@ public class Main {
         System.out.println("Press 1 to sort this array using Bubble Sort.!!");
         System.out.println("Press 2 to sort this array in Selection Sort.!!");
         System.out.println("Press 3 to sort this array in Insertion Sort.!!");
+        System.out.println("Press 3 to sort this array in Merge Sort.!!");
         int choice = scanner.nextInt();
         switch(choice){
             case 1: /*BUBBLE SORT*/
