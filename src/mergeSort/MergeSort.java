8d81c9ac300445660c9a966f94d47961d39fa760
Achyut Kumar Tiwari <achyuttiwari34@gmail.com>
2024-04-29 22:06:49 +0530

    function is to check weather the size of the array is greater than 1 or not if greater than we can divide the array by 2.

--- src/mergeSort/MergeSort.java
+++ src/mergeSort/MergeSort.java
@@ -3,5 +3,9 @@ package mergeSort;
 public class MergeSort {
 
     public void sort(int[] array, int sizeOfArray) {
+
+        if (sizeOfArray > 1) {
+            int halfSize = sizeOfArray / 2;
+        }
     }
 }
