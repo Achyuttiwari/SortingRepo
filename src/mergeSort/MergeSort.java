ee93fa07e04d0b6ae4dcf5bf849eef85784d1967
Achyut Kumar Tiwari <achyuttiwari34@gmail.com>
2024-05-02 23:32:51 +0530

    copy the value of the array in leftArray up to 0 to halfSize.

--- src/mergeSort/MergeSort.java
+++ src/mergeSort/MergeSort.java
@@ -9,6 +9,8 @@ public class MergeSort {
 
             int[] leftArray = new int[halfSize];
             int[] rightArray = new int[sizeOfArray - halfSize];
+
+            System.arraycopy(array, 0, leftArray, 0, halfSize);
         }
     }
 }
