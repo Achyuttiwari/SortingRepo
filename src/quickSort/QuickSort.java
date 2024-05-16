722d6401de987849fb7ecf5aa176d353d5a93f9f
Achyut Kumar Tiwari <achyuttiwari34@gmail.com>
2024-05-16 23:54:51 +0530

    the first element greater than the pivot

--- src/quickSort/QuickSort.java
+++ src/quickSort/QuickSort.java
@@ -3,5 +3,7 @@ package quickSort;
 public class QuickSort {
     public static void sort(int[] array, int leftIndex, int rightIndex) {
         int pivot = rightIndex;
+
+        int partitionIndex = leftIndex;
     }
 }
